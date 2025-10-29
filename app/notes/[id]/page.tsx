8d8ac66899be13d15a27cbc7b6b696b84d1718a4
kostia <natashatonyk493@gmail.com>
2025-10-29 22:48:00 +0200

    fix notedetails.client

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { dehydrate, QueryClient, HydrationBoundary } from "@tanstack/react-query";
 import {fetchNoteById} from "../../../lib/api"
-import NoteDetailsClient from "./NoteDetails"
+import NoteDetailsClient from "./NoteDetails.client"
 
 type Props = {
   params: { id: string };
